443852e948e733581724111e5eff56c94b9fda8a
Omar Adra <adra.omar2@gmail.com>
2026-04-07 19:44:41 -0300

    Cambios en Programa00

--- Programa00/Program.cs
+++ Programa00/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Mi nombre es Omar Adra");
+        Console.WriteLine("Estudio Ing. en Sistemas y Tecnicatura Universitaria en Programacion en la UTN-FRT");
+        Console.WriteLine("Actualmente trabajo en el departamento de IT como administrador de Redes en un Call Center");
+        Console.WriteLine("En mis tiempos libres suelo jugar al futbol en distintos torneos de tucuman");
+        Console.WriteLine("Estoy todo el tiempo ocupado, pero me doy mis momentos libres cuando puedo :D");
+    }
+}
+
